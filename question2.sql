fe8feadcfb96260a2c39bad6daba68eee88d85e4
sang-il <lsi9393@naver.com>
2024-01-18 19:27:34 +0900

    add question2.3

---
+++ question2.sql
@@ -0,0 +1,3 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59035
+
+select name, datetime from animal_ins order by animal_id desc
---
+++ question3.sql
@@ -0,0 +1,3 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59408
+
+select count(distinct name) from animal_ins
